b8ed206b7d594fc9591861552e777a521edbda20
Anton Doudarev <antonthedev@gmail.com>
2018-03-20 22:06:03 -0400

    Extracted the Easing enumerator from FlightAnimator, renamed it, and created frameworsk for iOS and tvOS

---
+++ FlightParametrics/FlightParametrics/FlightParametrics.h
@@ -0,0 +1,19 @@
+//
+//  FlightParametrics.h
+//  FlightParametrics
+//
+//  Created by Anton on 3/20/18.
+//  Copyright © 2018 Anton Doudarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlightParametrics.
+FOUNDATION_EXPORT double FlightParametricsVersionNumber;
+
+//! Project version string for FlightParametrics.
+FOUNDATION_EXPORT const unsigned char FlightParametricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlightParametrics/PublicHeader.h>
+
+
---
+++ FlightParametrics/FlightParametricsTests/FlightParametricsTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlightParametricsTests.swift
+//  FlightParametricsTests
+//
+//  Created by Anton on 3/20/18.
+//  Copyright © 2018 Anton Doudarev. All rights reserved.
+//
+
+import XCTest
+@testable import FlightParametrics
+
+class FlightParametricsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
